bf87ac6b6e798cf8d080abdf82cdb4776bc43bd9
Andrey <andreyversa@gmail.com>
2021-01-12 02:07:14 +0200

    update file main.cpp

--- src/runtime/main.cpp
+++ src/runtime/main.cpp
@@ -5,6 +5,7 @@
 
 #include "../parserXML/pub/smartBuffer.h"
 #include "../parserXML/pub/lexerXML.h"
+#include "../parserXML/pub/parserXML.h"
 
 using std::cout;
 using std::cin;
@@ -34,35 +35,48 @@ void test_smartBuffer() {
 
 }
 
-void test_parserXML() {
-
-	std::ofstream fout("d:\\project\\PROJECT\\resourses\\test.xml");
+void test_lexer() {
+	std::ofstream fout("d:\\project\\PROJECT\\resourses\\testLexer.xml");
 	std::shared_ptr<parserXML::SmartBuffer> ptrBuf(new parserXML::SmartBuffer("d:\\project\\PROJECT\\resourses\\NTX1000_original.xml"));
 	if(ptrBuf.use_count() != 1) {
 		cout << "pointer is invalid" << endl;
 		return;
 	}
-	parserXML::SmartBuffer::IteratorSmartB iter(ptrBuf->begin());
-
 	parserXML::LexerXML lexer(ptrBuf);
 	parserXML::TokenXML token;
+	while(token.mTypeToken != parserXML::TokenXML::token_XML::END_OF_FILE) {
+		token = lexer.getNextToken();
+		fout << token.tokenName() << " : " << token.lexemValue() << endl;
+	}
+}
 
-	for(unsigned int i = 0; i < 30; ++i)
-		while(token.mTypeToken != parserXML::TokenXML::token_XML::END_OF_FILE) {
-			token = lexer.getNextToken();
-			fout << token.tokenName() << " : " << token.lexemValue() << endl;
-		}
+void test_parserXML() {
 
+	std::ofstream fout("d:\\project\\PROJECT\\resourses\\testParser.xml");
+	std::shared_ptr<parserXML::SmartBuffer> ptrBuf(new parserXML::SmartBuffer("d:\\project\\PROJECT\\resourses\\NTX1000_original.xml"));
+	if(ptrBuf.use_count() != 1) {
+		cout << "pointer is invalid" << endl;
+		return;
+	}
+	
+	parserXML::ParserXML parser("d:\\project\\PROJECT\\resourses\\NTX1000_original.xml");
+	for(int i = 0; i < 10; i++){
+		parser.findElement();
+	}
 
 }
 
 int main(int argc, char* argv[]) {
 
 	//test_smartBuffer();
+	//test_lexer();
 	test_parserXML();
-
+	
+	
 	return 0;
 }
+
+
 
 /* notes
 	g++ -c -Ilib/bub lib/lib.cpp -o build/objects/lib.o
